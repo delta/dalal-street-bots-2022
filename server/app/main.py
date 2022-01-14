ed8929e4620877f3b1df503c847ca53cc2072647
kelpikz <ajithaprasad96@gmail.com>
2022-01-14 13:07:29 +0530

    feat: add grpc module

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+import grpc
+import proto_build.DalalMessage_pb2_grpc as DalalMessage_pb2_grpc
+from proto_build.actions.Login_pb2 import LoginRequest
 
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Dalal": "ToTheMoon"}
+if __name__ == "__main__":
+    print("Trying to connect with grpc")
+    try:
+        cert = open("grpc-server.crt").read().encode("utf8")
+        creds = grpc.ssl_channel_credentials(cert)
+        channel = grpc.secure_channel(
+            "localhost:8000",
+            creds,
+            options=(
+                (
+                    "grpc.ssl_target_name_override",
+                    "localhost",
+                ),
+            ),
+        )
+        action_stub = DalalMessage_pb2_grpc.DalalActionServiceStub(channel)
+        login_req = LoginRequest(email="bot", password="bot")
+        login_res = action_stub.Login(login_req)
+        print("got the resp : ", login_res)
+    except Exception as e:
+        print("err : ", e)
+
+# @app.get("/")
+# def read_root():
+#     return {"Dalal": "ToTheMoon"}
