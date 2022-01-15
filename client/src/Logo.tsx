990db64c9c2d5cb4f2f0806245442b912f57b0a1
siva2204 <sivaraman26r@gmail.com>
2022-01-15 23:53:15 +0530

    [small] clean up

--- client/src/Logo.tsx
+++ client/src/Logo.tsx
@@ -1,13 +1,7 @@
-import * as React from "react"
-import {
-  chakra,
-  ImageProps,
-  forwardRef,
-} from "@chakra-ui/react"
-import logo from "./logo.png"
+import * as React from 'react';
+import { chakra, ImageProps, forwardRef } from '@chakra-ui/react';
+import logo from './logo.png';
 
-
-export const Logo = forwardRef<ImageProps, "img">((props, ref) => {
-
-  return <chakra.img src={logo} ref={ref} {...props} />
-})
+export const Logo = forwardRef<ImageProps, 'img'>((props, ref) => {
+    return <chakra.img src={logo} ref={ref} {...props} />;
+});
