331abaad2b20dacb20cc726d09a468a20b99933f
kelpikz <ajithaprasad96@gmail.com>
2022-01-23 14:25:56 +0530

    fix(dependency): refactor get_connection dependency and fix few bugs

--- server/app/core/dependencies/database.py
+++ server/app/core/dependencies/database.py
@@ -1,8 +1,8 @@
 """Database dependencies"""
 
 from fastapi import Depends
-from starlette.requests import Request
-from aiomysql import Pool
+from fastapi import Request
+from aiomysql import Pool, Connection
 from aiomysql.cursors import Cursor
 
 
@@ -10,15 +10,24 @@ def _get_db_pool(request: Request) -> Pool:
     return request.app.state.pool
 
 
-def get_connection():
+async def _get_db_connection(pool: Pool = Depends(_get_db_pool)) -> Connection:
+    conn: Connection = await pool.acquire()
+    try:
+        yield conn
+    finally:
+        print("Closing connection")
+        conn.close()
+
+
+async def get_connection(conn=Depends(_get_db_connection)):
     """A FAST API Dependency which returns a cursor from the database
     connection pool. By using this function the user is guaranteed  to
     get a database cursor. Any route which requires some kind of database
     interation must use this dependency to access the database."""
 
-    async def get_cursor_from_pool(pool: Pool = Depends(_get_db_pool)):
-        conn = await pool.acquire()
-        curr: Cursor = await conn.cursor()
-        return curr
-
-    return get_cursor_from_pool
+    cursor: Cursor = await conn.cursor()
+    try:
+        yield (cursor)
+    finally:
+        print("Closing cursor")
+        await cursor.close()
