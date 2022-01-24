c2f7b48ff13a5bce71012232cb2373f986dbd682
kelpikz <ajithaprasad96@gmail.com>
2022-01-25 00:30:31 +0530

    feat(db): create a seperate file for useful errors

---
+++ server/app/db/errors.py
@@ -0,0 +1,5 @@
+"""Utility errors to check if the user"""
+
+RecordNotFound = Exception("Record Not found")
+InternalServer = Exception("Internal server error")
+DuplicateKey = Exception("Duplicate key error")
