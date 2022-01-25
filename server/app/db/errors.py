3f1c2d81523162c0655e24dfb5c1e12258b7e4ee
kelpikz <ajithaprasad96@gmail.com>
2022-01-25 17:09:52 +0530

    misc(db): add a todo in errors

--- server/app/db/errors.py
+++ server/app/db/errors.py
@@ -3,3 +3,7 @@
 RecordNotFound = Exception("Record Not found")
 InternalServer = Exception("Internal server error")
 DuplicateKey = Exception("Duplicate key error")
+
+# TODO: create a type of a error, eg for DuplicateKey Error,
+# we shd be able to duplicate field  id
+# eg, "Bot with name=ExampleBot already exists"
