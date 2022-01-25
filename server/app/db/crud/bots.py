b702d4753c5418ae5d40bb0d58c73d9c9795d5b0
kelpikz <ajithaprasad96@gmail.com>
2022-01-25 17:12:24 +0530

    feat(db): create functions to perform crud operations on bots along with corresponding schemas for data validation

---
+++ server/app/db/crud/bots.py
@@ -0,0 +1,265 @@
+"""Database layer for bots"""
+
+import logging
+from tkinter import E
+from typing import Any, List, Tuple, Union
+from aiomysql.cursors import Cursor
+from pydantic import ValidationError
+from pypika import Table, MySQLQuery, Criterion
+
+import db.schemas.bots as bots_schema
+from .base import log_query, log_query_with_arguments
+from .botTypes import get_bot_type_with_given_name
+
+
+bots, bot_types = Table("bots", "bot_types")
+
+
+async def create_bot(
+    con: Cursor, name=str, bot_type=Union[str, int]
+) -> Tuple[bool, Exception]:
+    """Creates a bot with the given name and bot_type.
+    given bot_type can be either a str(bot_type.name) or int(bot_type.id)
+
+    Returns True if the bot is successfully created, else (False, error)
+    _"""
+
+    bot_type_id: int = 0
+
+    logging.info(f"Trying to create a bot with {name=} {bot_type=}")
+
+    if type(bot_type) is int:
+        bot_type_id = bot_type
+    else:
+        # the bot_type name is given. get its id from bot_types
+        # and update bot_id
+        logging.debug("name of bot_type was given, fetching bot_type id from db")
+        (bot_type_data, err) = await get_bot_type_with_given_name(con, bot_type)
+        logging.debug(
+            f"Successfully found bot_type data={bot_type_data.dict()} when fetching"
+            + f"bot_type with name={bot_type}"
+        )
+        if err:
+            # Some error occurred, return the error to the user
+            return False, err
+        bot_type_id = bot_type_data.id
+
+    try:
+        data = bots_schema.CreateBot(name=name, bot_type=bot_type_id)
+    except ValidationError as e:
+        logging.Error(f"Create bot validation failed due to {e.errors()}")
+        return False, e
+
+    # Creating query
+    q = (
+        MySQLQuery.into(bots)
+        .columns("name", "bot_type")
+        .insert(data.name, data.bot_type)
+    )
+
+    log_query(q)
+
+    try:
+        # executing query
+        await con.execute(q)
+        await con.commit()
+        logging.info(f"Successfully created a bot with {name=} {bot_type=}")
+        return True, None
+    except Exception as e:
+        logging.error(
+            f"Couldn't create bot with {name=} {bot_type=} with query=`{q}` due to `{e}`"
+        )
+        return False, e
+
+
+async def get_all_bots(
+    con: Cursor, inflate_bot_type: bool = True
+) -> Tuple[
+    Union[List[bots_schema.BotInDB], List[bots_schema.BotInDBInflated]], Exception
+]:
+    """Finds and returns all the bots, optionally can pass on 'bot_type' to specify
+    if you want to find the corresponding bot_type details and return it.
+
+    Args:
+        con (Cursor): AioMySql Connection
+        inflate_bot_type (bool, optional): specify whether if you want to find the name of the
+        corresponding bot_type row and return during response. Defaults to True.
+
+    Returns:
+        Tuple[List[bots_schema.BotInDB], Exception]: List of all the bots, Exception if any
+    _"""
+
+    logging.info(f"Finding all bots with {inflate_bot_type=}")
+
+    q = (
+        MySQLQuery.from_(bots).select("*")
+        if inflate_bot_type is False
+        else (
+            "SELECT bots.*, \
+        bot_types.id as bot_type_id,\
+        bot_types.name as bot_type_name \
+        from bots INNER JOIN bot_types ON bots.bot_type=bot_types.id"
+        )
+    )
+
+    log_query(str(q))
+
+    try:
+        await con.execute()
+        rows = await con.fetchall()
+        resp = (
+            [bots_schema.create_botInDBInflated_from_tuple(row) for row in rows]
+            if bot_types is True
+            else [bots_schema.create_BotInDB_from_tuple(row) for row in rows]
+        )
+        return resp, None
+    except Exception as e:
+        logging.error(
+            f"Couldn't query all bots with {inflate_bot_type=} with query={q} due to {e}"
+        )
+        return [], e
+
+
+async def query_bots_with_details(
+    con: Cursor,
+    name: str = "",
+    bot_type: Union[str, int] = 0,
+    inflate_bot_type: bool = True,
+) -> Tuple[
+    Union[List[bots_schema.BotInDB], List[bots_schema.BotInDBInflated]], Exception
+]:
+    """Fetches all the bots with the queried details.
+    name - returns Bots[] with the given name (List of len 1)
+    bot_type - returns Bots[] of the given bot_type.
+    bot_type can be int(bot_type.id) or str(bot_type.name)
+
+    additionally inflate_bot_type parameter can be passed so that bot_type details
+    are fetched and returned to the user, this defaults to True (bot_type data is
+    always inflated)"""
+
+    if inflate_bot_type is True:
+        return await _get_bots_with_details_inflated(con, name, bot_type)
+    else:
+        return await _get_bots_with_details_uninflated(con, name, bot_type)
+
+
+async def _get_bots_with_details_inflated(
+    con: Cursor, name: str = "", bot_type: Union[str, int] = 0
+) -> Tuple[List[bots_schema.BotInDBInflated], Exception]:
+    """Function to query inflated details"""
+
+    logging.info(f"Quering bots with {name=} and {bot_type=} inflated")
+
+    # validating data
+    try:
+        data = bots_schema.QueryBot(name=name, bot_type=bot_type)
+    except ValidationError as e:
+        # validation failed, return errors
+        logging.error(
+            f"Couldn't to query bots with {name=} and {bot_type=}",
+            f" as validation failed with the error(s): {e.errors()}",
+        )
+        return [], e
+
+    bot_id = 0
+    if type(data.bot_type) is str:
+        # name of the bot_type is given, query for the bot_type_id and
+        # use the to query the bot data
+        bot_type_data, err = await get_bot_type_with_given_name(con, data.bot_type)
+        if err:
+            return [], err
+        bot_id = bot_type_data.id
+    else:
+        bot_id = data.bot_type
+
+    # create the query
+    # "as" doesn't have proper support in pypika,
+    # so we have to write our own query
+    q = "SELECT bots.*, \
+        bot_types.id as bot_type_id,\
+        bot_types.name as bot_type_name \
+        from bots INNER JOIN bot_types ON bots.bot_type=bot_types.id\
+        WHERE "
+
+    if data.get_query_on() == "name":
+        q += "bots.name=%s"
+        log_query_with_arguments(q, [data.name])
+    else:
+        q += "bots.bot_type=%s"
+        log_query_with_arguments(q, [bot_id])
+
+    try:
+        if data.get_query_on() == "name":
+            await con.execute(q, [data.name])
+        else:
+            await con.execute(q, [bot_id])
+        rows = await con.fetchall()
+        resp = [bots_schema.create_botInDBInflated_from_tuple(row) for row in rows]
+        return resp, None
+    except Exception as e:
+        logging.error(
+            f"Couldn't query bots (inflated) with {data.dict()} with query={q}",
+            f" and args={[data.name, bot_id]} due to {e}",
+        )
+        return [], None
+
+
+async def _get_bots_with_details_uninflated(
+    con: Cursor, name: str = "", bot_type: Union[str, int] = 0
+) -> Tuple[List[bots_schema.BotInDB], Exception]:
+    """Function to query uninflated details"""
+
+    logging.info(f"Quering bots with {name=} and {bot_type=} uninflated")
+
+    # validating data
+    try:
+        data = bots_schema.QueryBot(name=name, bot_type=bot_type)
+    except ValidationError as e:
+        # validation failed, return errors
+        logging.error(
+            f"Couldn't to query bots with {name=} and {bot_type=}",
+            f" as validation failed with the error(s): {e.errors()}",
+        )
+        return [], e
+
+    bot_id = 0
+    if type(data.bot_type) is str:
+        # name of the bot_type is given, query for the bot_type_id and
+        # use the to query the bot data
+        bot_type_data, err = await get_bot_type_with_given_name(con, data.bot_type)
+        if err:
+            return [], err
+        bot_id = bot_type_data.id
+    else:
+        bot_id = data.bot_type
+
+    criteria: List[Any] = []
+    if data.get_query_on() == "name":
+        criteria.append(bots.name == data.name)
+    else:
+        criteria.append(bots.bot_id == bot_id)
+
+    # creating query
+    # selecting * -> criteria contraint
+    q = MySQLQuery.from_("bots").select("*").Where(Criterion.all(criteria))
+
+    # executing the query
+    try:
+        await con.execute(q)
+        rows = await con.fetchall()
+        resp = [bots_schema.create_BotInDB_from_tuple(row) for row in rows]
+        return resp, None
+    except Exception as e:
+        logging.error(
+            f"Couldn't query bots(uninflated) with {data.dict()} with query={q} due to {e}",
+        )
+        return [], None
+
+
+# TODO
+# async def update_bot_with_id(con: Cursor):
+#     """"""
+
+
+# async def delete_bot_with_id(con: Cursor):
+#     """"""
---
+++ server/app/db/schemas/bots.py
@@ -0,0 +1,122 @@
+"""Contains all the schema for bots db layer,
+_"""
+
+from datetime import datetime
+from typing import Any, Dict, Literal, Union
+from pydantic import BaseModel, Field, root_validator
+from .timestamp import TimestampInDBPlugin
+
+
+class BotBase(BaseModel):
+    name: str
+    bot_type: int
+
+
+class BotInDB(BotBase, TimestampInDBPlugin):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class BotInDBInflated(BotInDB):
+    """Bot db with bot_type data inflated"""
+
+    # bot_type_id: int
+    bot_type_name: str
+
+
+class CreateBot(BotBase):
+    pass
+
+
+class QueryBot(BaseModel):
+    name: str = Field("")
+    bot_type: Union[str, int] = Field(0)
+
+    _query_on = ""
+
+    def get_query_on(cls) -> Literal["name", "bot_type"]:
+        return cls._query_on
+
+    @root_validator
+    def check_valid_queried(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        "Checks if either of name or bot_type is given"
+        is_valid_name = cls.name != ""
+        is_valid_bot_type = False
+        if type(cls.bot_type is str):
+            if cls.bot_type is "":
+                is_valid_bot_type = False
+            else:
+                is_valid_bot_type = True
+        else:
+            if cls.bot_type is 0:
+                is_valid_bot_type = False
+            else:
+                is_valid_bot_type = True
+
+        if not is_valid_name and not is_valid_bot_type:
+            helpful_error_message = (
+                f"query of `name='{cls.name}'` and `bot_type='{cls.bot_type}'` "
+                + f"is not valid. Provide one value for 'bot_type' or 'name'"
+            )
+            raise ValueError(helpful_error_message)
+        if is_valid_bot_type and is_valid_name:
+            # Both the values cannot be queried, only one can be queried at a time
+            # so return a error
+            helpful_error_message = (
+                f"query of `name='{cls.name}'` and `bot_type='{cls.bot_type}'` "
+                + f"is not valid. You can only query with either one value of 'bot_type' or 'name'. "
+                f"You cannot provide both values."
+            )
+            raise (helpful_error_message)
+        if is_valid_bot_type:
+            cls._query_on = "bot_type"
+        else:
+            cls._query_on = "name"
+        return values
+
+
+def create_BotInDB_from_tuple(data: tuple) -> BotInDB:
+    """Converts db row returned as tuple to pydantic model"""
+
+    """Row Structure
+        (id, name, bot_type, created_at, updated_at)
+        ( 0,    1,        2,          3,          4)"""
+
+    # TODO: Handle case when validation fails
+
+    return BotInDB(
+        id=data[0],
+        name=data[1],
+        bot_type=data[2],
+        created_at=datetime(data[3]),
+        updated_at=datetime(data[4]),
+    )
+
+
+def create_botInDBInflated_from_tuple(data: tuple) -> BotInDBInflated:
+    """Converts db row returned as tuple to a pydantic model"""
+
+    # BUG: We dont know what the columns will be, right now we are
+    # hard-coding the order given in crud/bots/get_all_bots query
+    # and this function will break (return unexpected result / fail validation)
+    # when we change the column order,
+    # POSSIBLE SOLUTION: cur.description() might provide some info
+    # on how to solve this issue, need to check it later
+    # (but the docs are non-existant, need to check source code )
+
+    """Row Structure
+        (id, name, bot_type, created_at, updated_at, bot_type_id, bot_type_name)
+        ( 0,    1,        2,          3,          3,           4,             5)
+        ### DIFFERENT FOR DIFFERENT QUERIES
+    """
+
+    return BotInDBInflated(
+        id=data[0],
+        name=data[1],
+        bot_type=data[2],
+        created_at=datetime(data[3]),
+        updated_at=datetime(data[4]),
+        bot_type_name=data[5],
+    )
