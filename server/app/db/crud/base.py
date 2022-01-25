28b05b98ff685e1d6ec227b802b574f9ecea1862
kelpikz <ajithaprasad96@gmail.com>
2022-01-25 17:11:33 +0530

    feat(db): create a method to log sql query along with args

--- server/app/db/crud/base.py
+++ server/app/db/crud/base.py
@@ -1,6 +1,12 @@
 import logging
+from typing import Any, List
 
 
 def log_query(query: str) -> None:
     logging.debug(f"Executing query:'{query}'")
+    return
+
+
+def log_query_with_arguments(query: str, args: List[Any]) -> None:
+    logging.debug(f"Executing query:`{query}` with {args=}")
     return
