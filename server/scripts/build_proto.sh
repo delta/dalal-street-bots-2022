fc6db5da1bc8a0d122c215bc63ef3636781ab714
kelpikz <ajithaprasad96@gmail.com>
2022-02-15 04:58:26 +0530

    feat(scripts): update scripts to generate stubs for proto files

--- server/scripts/build_proto.sh
+++ server/scripts/build_proto.sh
@@ -8,10 +8,10 @@ cd "$(dirname "$SCRIPT")/../"
 mkdir -p ./app/proto_build
 rm -rf ./app/proto_build/*
 
-python3 -m grpc_tools.protoc -I proto --python_out=app/proto_build/ --grpc_python_out=app/proto_build/ ./proto/*.proto
-python3 -m grpc_tools.protoc -I proto --python_out=app/proto_build/ ./proto/actions/*.proto
-python3 -m grpc_tools.protoc -I proto --python_out=app/proto_build/ ./proto/datastreams/*.proto
-python3 -m grpc_tools.protoc -I proto --python_out=app/proto_build/ ./proto/models/*.proto
+python3 -m grpc_tools.protoc -I proto --python_out=app/proto_build/ --mypy_out=app/proto_build/  --grpc_python_out=app/proto_build/ ./proto/*.proto
+python3 -m grpc_tools.protoc -I proto --python_out=app/proto_build/ --mypy_out=app/proto_build/  ./proto/actions/*.proto
+python3 -m grpc_tools.protoc -I proto --python_out=app/proto_build/ --mypy_out=app/proto_build/  ./proto/datastreams/*.proto
+python3 -m grpc_tools.protoc -I proto --python_out=app/proto_build/ --mypy_out=app/proto_build/  ./proto/models/*.proto
 
 egrep -rl "^from (actions|datastreams|models)" app/proto_build/ | grep ".py" \
     | xargs sed -r -i.bak 's/^from (actions|datastreams|models) import/from proto_build.\1 import/g'
