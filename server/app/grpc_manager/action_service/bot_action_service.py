1ace6c3dded15b27b976487e07f9ef031feae506
kelpikz <ajithaprasad96@gmail.com>
2022-02-23 21:44:37 +0530

    feat(grpc-manager): create a a class for bot-action-service

---
+++ server/app/grpc_manager/action_service/bot_action_service.py
@@ -0,0 +1,20 @@
+"""Class manager for all auth action service actions"""
+
+from datetime import datetime
+import logging
+from .base_action_service import BaseActionService
+
+from proto_build.actions.CreateBot_pb2 import CreateBotRequest, CreateBotResponse
+
+
+class AuthActionService(BaseActionService):
+    """Helper class to handle all bot-related grpc requests"""
+
+    async def create_bot(self, bot_user_id: int) -> CreateBotResponse:
+        """Create bot request"""
+
+        createBotRequest = CreateBotRequest(bot_user_id=str(bot_user_id))
+
+        resp: CreateBotResponse = await createBotRequest
+
+        return resp
