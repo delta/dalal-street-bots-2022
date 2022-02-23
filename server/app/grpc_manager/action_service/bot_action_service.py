3dd9e6a57b9fa75a4008c1a7bb1912fa8688ba15
kelpikz <ajithaprasad96@gmail.com>
2022-02-24 03:28:20 +0530

    feat(grpc-manager): send metadata along with create_bot request

--- server/app/grpc_manager/action_service/bot_action_service.py
+++ server/app/grpc_manager/action_service/bot_action_service.py
@@ -1,6 +1,4 @@
 """Class manager for all auth action service actions"""
-
-from datetime import datetime
 import logging
 from .base_action_service import BaseActionService
 
@@ -10,11 +8,16 @@ from proto_build.actions.CreateBot_pb2 import CreateBotRequest, CreateBotRespons
 class BotActionService(BaseActionService):
     """Helper class to handle all bot-related grpc requests"""
 
-    async def create_bot(self, bot_user_id: int) -> CreateBotResponse:
+    async def create_bot(self, bot_name: str) -> CreateBotResponse:
         """Create bot request"""
 
-        createBotRequest = CreateBotRequest(bot_user_id=str(bot_user_id))
+        createBotRequest = CreateBotRequest(bot_user_id=bot_name)
 
-        resp: CreateBotResponse = await createBotRequest
-
-        return resp
+        try:
+            resp: CreateBotResponse = await self.action_stub.CreateBot(
+                createBotRequest,
+                metadata=self.metadata.get_bot_meta_data(),
+            )
+            return resp
+        except Exception as e:
+            logging.error(e)
