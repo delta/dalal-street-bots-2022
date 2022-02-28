9302592c9921eb6ad80861a99a7f04592c631a18
kelpikz <ajithaprasad96@gmail.com>
2022-02-28 09:16:26 +0530

    fix: refactor app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter  ,Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import {
     ChakraProvider,
     Box,
@@ -7,7 +7,6 @@ import {
     VStack,
     Grid,
     theme,
-    Switch,
     Heading,
     FormControl,
     FormLabel,
@@ -17,75 +16,74 @@ import {
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
-
- export const App = () => (
+export const App = () => (
     <ChakraProvider theme={theme}>
-       <Box textAlign="center" fontSize="xl">
-       <Grid minH="100vh" p={3}>
-         <ColorModeSwitcher justifySelf="flex-end" />
-         <VStack spacing={8}>
-           <Logo h="40vmin" pointerEvents="none" />
-           <Text>Dalal To The Moon</Text>
-         </VStack>
-       </Grid>
-     </Box>
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}>
-          <Login/>
-        </Route>
-      </Routes>
-      <Routes>
-        <Route path="/login" element={<Login />}>
-          <App/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-        
-   </ChakraProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    </ChakraProvider>
 );
 
-const Login=() =>{
-  return (
-      <Box
-          w={['full','md']}
-          p={[8,10]}
-          mt={[20, '10vh']}
-          border={['none','1px']}
-          borderColor ={['','gray']}
-          borderRadius={15}
-          >
-          <VStack 
-              spacing={4}
-              align ='flex-start'
-              w='full'>
-                  <VStack>
-                      <Heading>Login</Heading>
-                      <Text>Enter e-mail and password</Text>
-                      </VStack>
-                      <FormControl>
-                          <FormLabel>
-                              E-mail address
-                          </FormLabel>
-                          <Input rounded ='none' variant = 'filled'/>
-                      </FormControl>
-                      <FormControl>
-                          <FormLabel>
-                              Password
-                          </FormLabel>
-                          <Input rounded ='none' variant = 'filled' type ='password'/>
-                      </FormControl>
-                      <Button rounded ='none'
-                      colorScheme='green'
-                      w={['full','auto']}>
-                          Login</Button>
-                      
-              </VStack>
-      </Box>
-  );
-}
+const HomePage = () => {
+    return (
+        <Box textAlign="center" fontSize="xl">
+            <Grid minH="100vh" p={3}>
+                <ColorModeSwitcher justifySelf="flex-end" />
+                <VStack spacing={8}>
+                    <Logo h="40vmin" pointerEvents="none" />
+                    <Text>Dalal To The Moon</Text>
+                    <Link to="/login">
+                        <Button
+                            rounded="none"
+                            colorScheme="green"
+                            w={['full', 'auto']}
+                        >
+                            Login
+                        </Button>
+                    </Link>
+                </VStack>
+            </Grid>
+        </Box>
+    );
+};
 
-
-
-
-
+const Login = () => {
+    return (
+        <Box
+            w={['full', 'md']}
+            p={[8, 10]}
+            mt={[20, '10vh']}
+            border={['none', '1px']}
+            borderColor={['', 'gray']}
+            borderRadius={15}
+        >
+            <VStack spacing={4} align="flex-start" w="full">
+                <VStack>
+                    <Heading>Login</Heading>
+                    <Text>Enter e-mail and password</Text>
+                </VStack>
+                <FormControl>
+                    <FormLabel>E-mail address</FormLabel>
+                    <Input rounded="none" variant="filled" />
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Password</FormLabel>
+                    <Input rounded="none" variant="filled" type="password" />
+                </FormControl>
+                <Button rounded="none" colorScheme="green">
+                    Login
+                </Button>
+                <Link to="/">
+                    <Button rounded="none" colorScheme="orange">
+                        back
+                    </Button>
+                </Link>
+            </VStack>
+        </Box>
+    );
+};
