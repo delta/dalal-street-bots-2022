8c769ce1d5ef6cb7a169e46a8d2be9bf4edabe71
kelpikz <ajithaprasad96@gmail.com>
2022-03-05 10:06:32 +0530

    feat(migration): create a migration to add server_bot_id to bots

---
+++ server/app/db/migrations/versions/caf85e6ee2c7_add_server_bot_id_to_bots.py
@@ -0,0 +1,30 @@
+"""add_server_bot_id_to_bots
+
+Revision ID: caf85e6ee2c7
+Revises: 976573dc98f2
+Create Date: 2022-03-05 09:56:17.324327
+
+"""
+from alembic import op
+import logging
+
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'caf85e6ee2c7'
+down_revision = '976573dc98f2'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    logging.info("running up migrations for add_server_bot_id_to_bots")
+    op.add_column("bots", sa.Column("server_bot_id", sa.Integer, nullable=False, unique=True))
+    pass
+
+
+def downgrade():
+    logging.info("running down migrations for add_server_bot_id_to_bots")
+    op.drop_column("bots", "server_bot_id")
+    pass
